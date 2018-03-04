b580c1f47997433425f8e2eafffe814fbcecc471
Will McLean <will@pollen.com.au>
2018-03-05 09:04:04 +1100

    Add a selctor component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Selector from './Selector.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">App title</h1>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">App title</h1>
+                </header>
+                <Selector />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Selector.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Selector extends Component {
+    render() {
+        return (
+            <div className="Selector">
+                <p>Selector</p>
+            </div>
+        );
+    }
+}
+
+export default Selector;
