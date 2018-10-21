10215e7b5498f2cab5fc8e4f523875a56b431c34
Will McLean <willmcl@gmail.com>
2018-10-22 07:26:38 +1100

    Move the draggable function.

--- src/Playground.js
+++ src/Playground.js
@@ -2,6 +2,61 @@ import React, { Component } from 'react';
 import PlaygroundCircle from './PlaygroundCircle.js';
 
 class Playground extends Component {
+    
+    draggable() {
+
+        console.log('called');
+
+        let objects = document.querySelectorAll( '.PlaygroundCircle' );
+
+        let selected = null, // Object of the element to be moved
+            x_pos = 0, y_pos = 0, // Stores x & y coordinates of the mouse pointer
+            x_elem = 0, y_elem = 0, // Stores top, left values (edge) of the element
+            z = 7;
+
+        Array.prototype.forEach.call(objects, function (object) {
+
+            // Will be called when user starts dragging an element
+            function _drag_init( elem ) {
+                // Store the object of the element which needs to be moved
+                selected = elem;
+                selected.style.zIndex = z;
+                z++;
+                x_elem = x_pos - selected.offsetLeft;
+                y_elem = y_pos - selected.offsetTop;
+            }
+
+            // Will be called when user dragging an element
+            function _move_elem( e ) {
+                x_pos = document.all ? window.event.clientX : e.pageX;
+                y_pos = document.all ? window.event.clientY : e.pageY;
+                if ( selected !== null ) {
+                    selected.style.left = (x_pos - x_elem) + 'px';
+                    selected.style.top = (y_pos - y_elem) + 'px';
+                }
+            }
+
+            // Destroy the object when we are done
+            function _destroy() {
+                selected = null;
+            }
+
+            // Bind the functions...
+            object.onmousedown = function () {
+                _drag_init( this );
+                return false;
+            };
+
+            document.onmousemove = _move_elem;
+            document.onmouseup = _destroy;
+
+        });
+    }
+
+    componentDidMount() {
+        this.draggable();
+    }
+
     render() {
         return (
             <div className="Playground">
